4a497bc8b2daafbd6ff34eaf22ac208382c671f6
Savraj Singh <savrajsingh@gmail.com>
2019-01-03 10:18:27 -0500

    Part 3

---
+++ pokedex/src/components/Pokelist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Pokelist.css';
+
+const Pokelist = () => {
+    return(
+        <section className='poke-list'>
+        
+        </section>
+    )
+};
+
+export default Pokelist;
